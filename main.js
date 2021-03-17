80d63c96e66dee12bf0942cfa16ddf71293d7773
edwincaceres24 <edwin.caceres@pucp.pe>
2021-03-16 23:53:02 -0500

    Second Commiti, fix alert function

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const   API_URL = "https://pokeapi.co/api/v2/",
         POKEMON_URL = "pokemon/:id",
         opts = { crossDomain: true },
-        pokeContainer= $('#pokeContainer'); 
+        pokeContainer= $('#pokeContainer'),
+        pokemonCard=()=>document.querySelectorAll(".single--container");
+
         onPokemonResponse=function(pokemon){
 
                 let myVar= new Pokemon(`${pokemon.name}`,`${pokemon.types[0]['type']['name']}`)
+                myVar.createPokemonContainer()
+                myVar.getImage(`${pokemon.id}`)
                 myVar.sayHi()
-                myVar.sayType(`${pokemon.types[0]['type']['name']}`)
-                // console.log(`Hola, soy el pokemon ${pokemon.name}`,
-                //     mi experiencia es de ${pokemon.base_experience}`)
-                //console.table(`${pokemon.name}`)
+                myVar.pokemonCardFunction()
+                pokemonCard()
+                // myVar.sayType(`${pokemon.types[0]['type']['name']}`)
             };
 
 
@@ -22,26 +25,42 @@ const   btn =  $("#choose-pokemon")[0].innerText,    //document.getElementById("
 //Class
 
 class Pokemon{
-    constructor(name,types){ //The third sprit element has the default image
+    constructor(name,types,id){ //The third sprit element has the default image
         (this.name=name),
-        (this.types=types)}
-
-    sayHi(){
-            let newParagraph = document.createElement(`p`);
-            newParagraph.classList.add("pokemon--text");
-            newParagraph.innerHTML = (`Hola, soy el pokemon <span class="pokemon--name">${this.name}</span> y soy del tipo ${this.types}`);
-            pokeContainer.append(newParagraph)
-        }
-    sayType(types){
-            let newParagraph = document.createElement("p");
-            newParagraph.classList.add("pokemon--text");
-            newParagraph.innerHTML= (`Soy del tipo <span class="pokemon--name">${types}</span>`);
-            pokeContainer.append(newParagraph)
+        (this.types=types),
+        (this.id=id)}
+    
+    createPokemonContainer(){
+        let newContainer= document.createElement("article")
+        newContainer.classList.add("single--container")
+        pokeContainer.append(newContainer)
     }
+    sayHi(){
+            let newList = document.createElement(`li`);
 
+            newList.classList.add("pokemon--text");
+            newList.innerHTML = (`<ul>Nombre: <span class="pokemon--name">${this.name}</span></ul> 
+                                      <ul>Tipo: <span class="pokemon--name">${this.types}</span></ul>`);
+            pokeContainer[0].lastElementChild.append(newList)
+        }
+    pokemonCardFunction=function(){
+            let myVar = document.querySelectorAll(".single--container")
+            let alertMessage= ()=>alert(`Èste pokemon es de tipo ${this.types}`)
+            const clickOption = function(value){value.addEventListener("click",alertMessage)}
+            Array.from(myVar).map(clickOption)
+    }
+    
+    getImage(num){
+        let newImage= document.createElement("picture")
+        newImage.innerHTML=`<img src="https://pokeres.bastionbot.org/images/pokemon/${num}.png" class="img-container"/>`;
+        pokeContainer[0].lastElementChild.append(newImage)
+    }
 }
 
-//New Elementes
+//Events
+    
+
+
 
 
 
