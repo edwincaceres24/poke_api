f8aa76ac659363e4f1e7a7a27e15270009090c1a
edwincaceres24 <edwin.caceres@pucp.pe>
2021-04-01 18:08:03 -0500

    Commit from 01.03.21

--- main.js
+++ main.js
@@ -1,69 +1,67 @@
 const   API_URL = "https://pokeapi.co/api/v2/",
+
         POKEMON_URL = "pokemon/:id",
         opts = { crossDomain: true },
-        pokeContainer= $('#pokeContainer'),
+        pokeContainer= document.getElementById("pokeContainer"),
         pokemonCard=()=>document.querySelectorAll(".single--container");
-
         onPokemonResponse=function(pokemon){
-
-                let myVar= new Pokemon(`${pokemon.name}`,`${pokemon.types[0]['type']['name']}`)
-                myVar.createPokemonContainer()
-                myVar.getImage(`${pokemon.id}`)
-                myVar.sayHi()
-                myVar.pokemonCardFunction()
+                let pokeVar= new Pokemon(`${pokemon.name}`,`${pokemon.types[0]['type']['name']}`,`${pokemon.id}`)
+                pokeVar.createPokemonContainer()
+                pokeVar.getImage(`${pokemon.id}`)
+                pokeVar.getPokemonInfo()
+                pokeVar.pokemonCardFunction()
                 pokemonCard()
                 // myVar.sayType(`${pokemon.types[0]['type']['name']}`)
             };
-
-
-const   btn =  $("#choose-pokemon")[0].innerText,    //document.getElementById("choose-pokemon")
+const   btn =  document.getElementById("choose-pokemon").innerText,    //document.getElementById("choose-pokemon")
         showPokemon = function(id){
             $.get(`${API_URL}${POKEMON_URL.replace(':id',id)}`,opts,onPokemonResponse)
         },
         sortIdPokemon= () => Math.floor(Math.random()*150);
 
+//Async Function
+async function getPokemon(id){
+    const response= await fetch(`${API_URL}${POKEMON_URL.replace(':id',id)}`)
+    const data = await response.json()
+    .then (response =>onPokemonResponse(id) )
+}
+async function init(id){
+    console.log(await getPokemon(id))
+}
 //Class
-
 class Pokemon{
     constructor(name,types,id){ //The third sprit element has the default image
         (this.name=name),
         (this.types=types),
         (this.id=id)}
-    
     createPokemonContainer(){
         let newContainer= document.createElement("article")
         newContainer.classList.add("single--container")
         pokeContainer.append(newContainer)
     }
-    sayHi(){
+    getPokemonInfo(){
             let newList = document.createElement(`li`);
-
             newList.classList.add("pokemon--text");
-            newList.innerHTML = (`<ul>Nombre: <span class="pokemon--name">${this.name}</span></ul> 
-                                      <ul>Tipo: <span class="pokemon--name">${this.types}</span></ul>`);
-            pokeContainer[0].lastElementChild.append(newList)
+            newList.innerHTML = (`
+                                    <ul>Id: <span class="pokemon--name pokemon--id">${this.id}</span></ul>
+                                    <ul>Nombre: <span class="pokemon--name">${this.name}</span></ul> 
+                                    <ul>Tipo: <span class="pokemon--name">${this.types}</span></ul>`);
+
+            pokeContainer.lastElementChild.append(newList)
         }
     pokemonCardFunction=function(){
-            let myVar = document.querySelectorAll(".single--container")
-            let alertMessage= ()=>alert(`Èste pokemon es de tipo ${this.types}`)
+            let selectedClass = document.querySelectorAll(".single--container") //Se debe crear un modal dentro de la función constructora
+            let alertMessage= (e)=>{
+                let valueDesired = e.currentTarget.querySelector(".pokemon--id").innerText
+                alert (`Este pokemon tiene un Id de ${valueDesired}`)
+            }
             const clickOption = function(value){value.addEventListener("click",alertMessage)}
-            Array.from(myVar).map(clickOption)
+            Array.from(selectedClass).map(clickOption)
     }
-    
     getImage(num){
         let newImage= document.createElement("picture")
         newImage.innerHTML=`<img src="https://pokeres.bastionbot.org/images/pokemon/${num}.png" class="img-container"/>`;
-        pokeContainer[0].lastElementChild.append(newImage)
+        pokeContainer.lastElementChild.append(newImage)
     }
 }
-
 //Events
-    
-
-
-
-
-
-
-
-
